a7632f51c05fe18b0f014c5df8dd63e603d6950a
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-03-26 09:54:30 -0600

    changed size

--- src/artView/ArtFrame.java
+++ src/artView/ArtFrame.java
@@ -8,7 +8,7 @@ public class ArtFrame extends JFrame
 	
 	private void setupFrame()
 	{
-		this.setSize(500, 500);
+		this.setSize(800, 800);
 		this.setTitle("DrawingProject");
 		//this.setContentPane(appPanel);
 		this.setResizable(true);
