a8f3d200c1c86934ab8bc5610841baa6d7198f21
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-03-26 09:54:11 -0600

    Made some changes to frame

--- src/artView/ArtFrame.java
+++ src/artView/ArtFrame.java
@@ -1,5 +1,18 @@
 package artView;
 
-public class ArtFrame {
+import javax.swing.JFrame;
 
+public class ArtFrame extends JFrame
+{
+
+	
+	private void setupFrame()
+	{
+		this.setSize(500, 500);
+		this.setTitle("DrawingProject");
+		//this.setContentPane(appPanel);
+		this.setResizable(true);
+		this.setVisible(true);
+		
+	}
 }
