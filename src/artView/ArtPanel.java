0d5dbcdf2a645739442fae3bbb02c26e5f27090c
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-04-10 08:53:45 -0600

    added more stuff

--- src/artView/ArtPanel.java
+++ src/artView/ArtPanel.java
@@ -133,6 +133,26 @@ public class ArtPanel extends JPanel
 	{
 		
 	}
+	private boolean coinFlip()
+	{
+		return (int)(Math.random()*2)==0;
+	}
+	private Polygon createPolygon(int sides)
+	{
+		Polygon currentShape = new Polygon();
+		int originX = (int)(Math.random() * 600);
+		int originY = (int)(Math.random() * 600);
+		
+		for(int index = 0; index < sides; index++)
+		{
+			int minus = coinFlip()? -1 : 1;
+			int shiftX = (int)(Math.random() * currentScale) * minus;
+			minus = coinFlip() ? -1 : 1;
+			int shiftY = (int)(Math.random() * currentScale) * minus;
+			currentShape.addPoint(originX + shiftX, originY + shiftY);
+		}
+		return currentShape;
+	}
 	private void setupListeners()
 	{
 		
