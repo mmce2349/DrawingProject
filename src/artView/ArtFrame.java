1f8647ef344e8d7bbd247763c53f5d46c8034118
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-04-10 08:29:12 -0600

    more stuff to do

--- src/artView/ArtFrame.java
+++ src/artView/ArtFrame.java
@@ -1,10 +1,16 @@
 package artView;
 
 import javax.swing.JFrame;
-
+import artController.ArtController;
 public class ArtFrame extends JFrame
 {
-
+	private ArtPanel appPanel;
+	public ArtFrame(ArtController app)
+	{
+		super();
+		appPanel= new ArtPanel(app);
+		setupFrame();
+	}
 	
 	private void setupFrame()
 	{
